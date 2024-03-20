2f23353fa0b15c28fa4a318cbb200f3b0d7c07ec
Grégoire Chapeaux <gregoire.chapeaux@apexo.net>
2024-03-20 10:55:26 -0400

    Update for more readable bug.

--- ResponsivePage.xaml.cs
+++ ResponsivePage.xaml.cs
@@ -3,9 +3,13 @@
     public partial class ResponsivePage : ContentPage
     {
 
+        bool UseUserRotation = false;
+
         public ResponsivePage()
         {
             InitializeComponent();
+
+            MDIOLabel.Text = "MainDisplayInfo.Orientation : " + DeviceDisplay.Current.MainDisplayInfo.Orientation.ToString();
 
             RespModeLabel.Text = (DeviceDisplay.Current.MainDisplayInfo.Orientation == DisplayOrientation.Portrait) ? "I'm in Portrait mode" : "I'm in Landscape Mode";
 
@@ -27,9 +31,13 @@
         protected override void OnAppearing()
         {
             base.OnAppearing();
+            
 #if ANDROID
-        SOToRestore = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.RequestedOrientation;
-        Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.RequestedOrientation = Android.Content.PM.ScreenOrientation.FullSensor;
+            SOToRestore = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.RequestedOrientation;
+            Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.RequestedOrientation = Android.Content.PM.ScreenOrientation.User;
+            UseUserRotation = true;
+            ROLabel.Text = (UseUserRotation) ? "RequestedOrientation : User" : "RequestedOrientation : Portrait";
+            Console.WriteLine(DeviceDisplay.Current.MainDisplayInfo.Orientation.ToString());
 #endif
         }
 
@@ -38,12 +46,22 @@
             base.OnDisappearing();
 #if ANDROID
         if (SOToRestore != null && SOToRestore is Android.Content.PM.ScreenOrientation SO) Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;
+        UseUserRotation = false;
 #endif
         }
 
         private void ModeBtn_Clicked(object sender, EventArgs e)
         {
-            ModeBtn.Text = DeviceDisplay.Current.MainDisplayInfo.Orientation.ToString();
+            MDIOLabel.Text = "MainDisplayInfo.Orientation : " + DeviceDisplay.Current.MainDisplayInfo.Orientation.ToString();
+        }
+
+        private void SwitchModeBtn_Clicked(object sender, EventArgs e)
+        {
+#if ANDROID
+            Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.RequestedOrientation = (UseUserRotation) ? Android.Content.PM.ScreenOrientation.Portrait : Android.Content.PM.ScreenOrientation.User;
+            UseUserRotation = !UseUserRotation;
+            ROLabel.Text = (UseUserRotation) ? "Current RequestedOrientation : User" : "Current RequestedOrientation : Portrait";
+#endif
         }
     }
 }
